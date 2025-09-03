38c2bb76b11d12dca54b31aa60934d37dec28557
deepakmauryacs <62381959+deepakmauryacs@users.noreply.github.com>
2025-09-03 16:49:01 +0530

    Fix CSRF token usage for captcha refresh

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Login</title>
-  <meta name="csrf-token" content="dqiXSHcP7rwGlEqKXIvz82w3pAIt7OEs7OQScHqU">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <!-- Fonts & Vendors -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
