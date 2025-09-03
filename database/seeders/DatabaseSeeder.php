e247091506f4c647c0ae1ed89ff964cd881ada72
Dev <dm.raprocuretesting@gmail.com>
2025-09-03 18:36:18 +0530

    fix issue

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,8 +6,6 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-// Additional seeders
-use Database\Seeders\CountrySeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,14 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(CountrySeeder::class);
-
-        // Example user for testing purposes
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-
         // Admin user
         User::factory()->create([
             'name' => 'Admin',
