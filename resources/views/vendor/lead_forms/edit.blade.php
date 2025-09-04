fb3c5aaa354a8c800c6cd9c45eb26235d79d4848
deepakmauryacs <62381959+deepakmauryacs@users.noreply.github.com>
2025-09-05 00:14:54 +0530

    Replace prompts with modal in lead form builder

--- resources/views/vendor/lead_forms/edit.blade.php
+++ resources/views/vendor/lead_forms/edit.blade.php
@@ -35,6 +35,31 @@
   </form>
 </div>
 
+<div class="modal fade" id="fieldModal" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog">
+    <form class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Field Details</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="mb-3">
+          <label class="form-label">Label</label>
+          <input type="text" class="form-control" id="field-label">
+        </div>
+        <div class="mb-3" id="options-wrapper">
+          <label class="form-label">Options (comma separated)</label>
+          <input type="text" class="form-control" id="field-options">
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+        <button type="submit" class="btn btn-primary">Save</button>
+      </div>
+    </form>
+  </div>
+</div>
+
 <script>
   window.existingFields = @json($form->fields ?? []);
 </script>
